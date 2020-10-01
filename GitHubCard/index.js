e84eeb61d0ec46d2b94d8a779f58c0d1a0a1bc90
adam-rappaport <adamrappaport@icloud.com>
2020-10-01 18:51:53 -0500

    still working on last question

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,101 @@
+
+import axios from 'axios'
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
+
+  
 */
+
+const boxCards = document.querySelector('.cards')
+
+function adamCard(data){
+  
+  const cardDiv = document.createElement('div')
+  const picture = document.createElement('img')
+  const header = document.createElement('h2')
+  const para = document.createElement('p')
+
+
+picture.src = data.avatar_url
+
+header.textContent = data.login
+para.textContent = data.location
+
+
+boxCards.appendChild(cardDiv)
+cardDiv.appendChild(picture)
+cardDiv.appendChild(header)
+cardDiv.appendChild(para)
+
+
+
+
+
+}
+
+axios.get('https://api.github.com/users/adamrappaport')
+.then(res=>{
+  console.log(res.data)
+  const data = res.data
+  adamCard(data)
+
+})
+.catch(drama =>{
+  console.log(drama)
+})
+
+
+
+ const followersArray = [
+   "CScori",
+   "Matte-matt",
+   "Benaiah-Varner",
+   "s-munro",
+   "roberttierie"
+
+ ];
+
+ followersArray.map(item =>{
+   axios.get(`https://api.github.com/users/${item}`)
+   .then(res=>{
+     adamCard(res)
+     console.log(res)
+   })
+
+   .catch(err=>{
+     console.log(err)
+   })
+ })
+
+
+ function adamCard2(data){
+  
+  const cardDiv = document.createElement('div')
+  const picture = document.createElement('img')
+  const header = document.createElement('h2')
+  const para = document.createElement('p')
+
+
+picture.src = data.avatar_url
+
+header.textContent = data.login
+para.textContent = data.location
+
+
+boxCards.appendChild(cardDiv)
+cardDiv.appendChild(picture)
+cardDiv.appendChild(header)
+cardDiv.appendChild(para)
+
+
+boxCards.appendChild(cardDiv)
+
+
+}
+
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -11,6 +104,7 @@
 
     Skip to STEP 3.
 */
+
 
 /*
   STEP 4: Pass the data received from Github into your function,
@@ -28,7 +122,7 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
